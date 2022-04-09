f056ef2fff92069037fb293d17a802d8c2ac4c89
Owusu-Desmond <desmond0406114009@gmail.com>
2022-04-10 01:29:16 +0700

    styling item component

--- shopping-app/src/Item.js
+++ shopping-app/src/Item.js
@@ -1,16 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Item.css"
 
 function Item({item, onAddToCart}){
     return (
         <div className="Item">
             <div className="Item-left">
                 <div className="Item-image"></div>
-                <div className="Item-title">
-                    {item.name}
-                </div>
-                <div className="Item-discription">
-                    {item.discription}
+                <div className="Item-title-discription">
+                    <div className="Item-title">
+                        {item.name}
+                    </div>
+                    <div className="Item-discription">
+                        {item.discription}
+                    </div>
                 </div>
             </div>
             <div className="Item-right">
