ef53ba0fd15a83015e9716fd191c429fe905f035
Owusu-Desmond <desmond0406114009@gmail.com>
2022-05-21 18:55:29 +0000

    adding items controls to cartPage

--- shopping-app/src/CartPage.js
+++ shopping-app/src/CartPage.js
@@ -1,18 +1,33 @@
 import uuid from 'react-uuid';
-import React from 'react';
+import React, { useState } from 'react';
 import Prototypes from 'prop-types';
 import Item from './Item';
 import './CartPage.css';
 function CartPage({items}){
+    const [count, setCount] = useState(1);
+    const addOne = () => {
+        if(count < 10){
+           setCount(count + 1); 
+        }
+        
+    }
+    const removeOne = () => {
+        if(count > 1){
+            setCount(count - 1); 
+         }
+    }
     return (
         <ul  className='CartPage-items'>
             {
                 items.map((item) => 
                     <li key={uuid()} className='CartPage-item'>
-                        <Item item={item}/>
-                        {/* <div>
-                         <button>Remove</button>
-                        </div>  */}
+                        <Item item={item} count={count}>
+                            <div className='Item-controls'>
+                                <button onClick={() => removeOne()} className="Item-controls-button">-</button>
+                                <button className='Number-of-item'>{count}</button>
+                                <button onClick={() => addOne()} className="Item-controls-button">+</button>
+                            </div>
+                        </Item>
                     </li>   
                 )
             }
