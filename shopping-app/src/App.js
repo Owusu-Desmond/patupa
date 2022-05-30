222fb46548613ff09089d2cb1849d648a9ee1677
Owusu-Desmond <desmond0406114009@gmail.com>
2022-05-30 00:42:18 +0000

    make cartPage controls function

--- shopping-app/src/App.js
+++ shopping-app/src/App.js
@@ -14,6 +14,20 @@ const App = () => {
     setCart(previous => {
      return [...previous, item];
     })
+    console.log(cart);
+  }
+  const removeItem = (item) => {
+    let itemIndex = cart.findIndex(i => i.id === item.id);
+    if (itemIndex >= 0) {
+      console.log(200);
+        setCart(cart => {
+          const cartCopy = [...cart];
+          cartCopy.splice(itemIndex,1);
+          return cartCopy;
+        }
+      );
+    };
+
   }
   return (
     <div className='App'>
@@ -25,11 +39,12 @@ const App = () => {
         {summarizeCart(cart).length} items
       </div>
       <main className='App-content'>
-        <Content tab={activeTab} onAddToCart={addToCart} cart={summarizeCart(cart)} />
+        <Content tab={activeTab} onAddToCart={addToCart} onRemoveItem={removeItem} cart={summarizeCart(cart)} />
       </main>
     </div>
   )
 }
+// summarize cart to ignore already added items
 const summarizeCart = (cart) => {
   const groupItems = (cart) => cart.reduce((summary, item) => {
     summary[item.id] = summary[item.id] || {
@@ -41,12 +56,13 @@ const summarizeCart = (cart) => {
   },{})
   return Object.values(groupItems(cart));
 }
-const Content = ({cart,tab, onAddToCart}) =>{
+// switch app content to components
+const Content = ({cart,tab, onAddToCart , onRemoveItem}) =>{
   switch (tab) {
     case 'items':
       return <span> <ItemPage items={items} onAddToCart={onAddToCart}/></span>;
     case 'carts':
-      return <span> <CartPage items={summarizeCart(cart)}/> </span>;
+      return <span> <CartPage items={cart} onRemoveOne={onRemoveItem} onAddOne={onAddToCart}/> </span>;
     case 'home':
       return <span> <HomePage /></span>
     default: 
