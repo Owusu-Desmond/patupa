48d64810a5c010a532629a8e7e6221a5ca644e17
Owusu-Desmond <desmond0406114009@gmail.com>
2022-07-26 23:23:00 +0000

    Re-structure cart page

--- src/CartPage.js
+++ src/CartPage.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Item from './Item';
-import './products.css';
+import './CartPage.css';
 
 function CartPage({ items, onAddOne, onRemoveOne }) {
   return (
-    <div className="products-container">
-      {
-        items.map((item) => (
-          <div key={item.id} className="product-card">
-            <Item item={item}>
-              <div className="product-controls">
-                <button type="button" onClick={() => onRemoveOne(item)} className="Item-controls-button">-</button>
-                <button type="button">{item.count}</button>
-                <button type="button" onClick={() => onAddOne(item)} className="Item-controls-button">+</button>
-              </div>
-            </Item>
-          </div>
-        ))
-      }
+    <div className="cart-container">
+      <div>
+        <ul className="cart-products-container">
+          {
+            items.map((item) => (
+              <li key={item.id} className="cart-product-card">
+                <Item item={item}>
+                  <div className="product-controls">
+                    <button type="button" onClick={() => onRemoveOne(item)}>-</button>
+                    <button type="button" className="product-control-count">{item.count}</button>
+                    <button type="button" onClick={() => onAddOne(item)}>+</button>
+                  </div>
+                </Item>
+              </li>
+            ))
+          }
+        </ul>
+        <p className="cart-total-cost">
+          <span>Total Cost :</span>
+          <span>
+            $
+            {
+              items.reduce((toatalCost, item) => toatalCost + (item.count * item.price), 0)
+            }
+          </span>
+        </p>
+      </div>
+      <div className="payment-details-container">
+        <h3> Payment Details </h3>
+      </div>
     </div>
   );
 }
