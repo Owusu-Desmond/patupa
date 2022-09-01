9c1061f6b36ec3ce4f7f3b9ad72da1e7097f18e4
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-01 02:53:14 +0000

    Refactor nav

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -4,7 +4,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import PropTypes from 'prop-types';
 // import logo from '../images/logo.jpg';
 import '../css/App.css';
 
@@ -19,16 +18,29 @@ const AppNav = () => {
   return (
     <Navbar className="navbar fs-5" expand="lg">
       <Container>
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand className="nav-bland">
+          <NavLink to="/">Patupa.com</NavLink>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link>
-              <NavLink to="/products">Products</NavLink>
+              <NavLink to="/categories">Categories</NavLink>
             </Nav.Link>
             <NavDropdown
-              title="About"
+              title="Customer service"
               id="basic-nav-dropdown"
+              className="nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Contant Us</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Ordering</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Shipping && Delivery</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Gift Cards</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown
+              title="About Patupa"
+              id="basic-nav-dropdown"
+              className="nav-dropdown"
               show={show}
               onMouseEnter={showDropdown}
               onMouseLeave={hideDropdown}
@@ -39,20 +51,26 @@ const AppNav = () => {
               <NavDropdown.Item href="#blog">Blog</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown
-              title="Customer service"
+              title="International"
               id="basic-nav-dropdown"
+              className="nav-dropdown"
             >
-              <NavDropdown.Item href="#action/3.1">Contant Us</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Ordering</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Shipping && Delivery</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Gift Cards</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">France</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">United Kingdom</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">United State</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text className="btn btn-light">
-            Login
-          </Navbar.Text>
+          <NavDropdown
+            title="Welcome, Desmond !"
+            id="basic-nav-dropdown"
+            className="nav-user-dropdown"
+          >
+            <NavDropdown.Item href="#action/3.1">My account</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Track my order</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">logout</NavDropdown.Item>
+          </NavDropdown>
         </Navbar.Collapse>
       </Container>
     </Navbar>
