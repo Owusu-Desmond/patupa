d1cfe2c0b83cb88ad3781ba0de5476a9541432cc
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-13 17:22:04 +0000

    Implement authentication and use redux state

--- src/App.js
+++ src/App.js
@@ -1,65 +1,51 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Nav from './components/AppNav';
+import Auth from './Auth/Auth';
 import './css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ScrollToTop from './scrollToTop';
 import HomePage from './pages/HomePage';
 import CartPage from './pages/CartPage';
 import Footer from './components/footer';
 import ProductPage from './pages/product-page';
 import Categories from './components/categories';
+import Callback from './components/callback';
 
 const App = () => {
+  // call the useslector hook to get the products from the store
   const products = useSelector((state) => state.products);
-  const [cart, setCart] = useState([]);
-  // add Items to cart
-  const addToCart = (item) => {
-    setCart((previous) => [...previous, item]);
-  };
-  const removeOne = (item) => {
-    const itemIndex = cart.findIndex((i) => i.id === item.id);
-    if (itemIndex >= 0) {
-      setCart((cart) => {
-        const cartCopy = [...cart];
-        cartCopy.splice(itemIndex, 1);
-        return cartCopy;
-      });
-    }
-  };
-  // summarize cart to ignore already added items
-  const summarizeCart = (cart) => {
-    let alreadySummarized;
-    const groupItems = (cart) => cart.reduce((summary, item) => {
-      alreadySummarized = summary;
-      alreadySummarized[item.id] = alreadySummarized[item.id] || {
-        ...item,
-        count: 0,
-      };
-      alreadySummarized[item.id].count += 1;
-      return summary;
-    }, {});
 
-    return Object.values(groupItems(cart));
-  };
+  const auth = new Auth();
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Nav />
-        <main className="App-content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/cart" element={<CartPage cartItems={summarizeCart(cart)} onRemoveOne={removeOne} onAddOne={addToCart} onAddToCart={addToCart} />} />
-            {products.map((product) => (
-              <Route key={product.id} path={`/product/id=${product.id}`} element={<ProductPage product={product} onAddToCart={addToCart} />} />
-            ))}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Nav auth={auth} />
+      <main className="App-content">
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/categories" element={<Categories />} />
+          {/* if user is authenticated allow access to cart page else redirect to homepage */}
+          {auth.isAuthenticated() ? (
+            <>
+              <Route path="/cart" element={<CartPage />} />
+            </>
+          ) : (
+            <>
+              <Route path="/cart" element={<HomePage />} />
+            </>
+          )}
+
+          {products.map((product) => (
+            <Route key={product.id} path={`/product/id=${product.id}`} element={<ProductPage product={product} auth={auth} />} />
+          ))}
+          <Route path="/callback" element={<Callback auth={auth} />} />
+        </Routes>
+        <ScrollToTop />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
