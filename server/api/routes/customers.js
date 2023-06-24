2b2de4fc0c49ebfad302941aae590a188996c937
Owusu-Desmond <desmond0406114009@gmail.com>
2023-06-24 02:01:40 +0000

    Setup products and customers routes

--- server/api/routes/customers.js
+++ server/api/routes/customers.js
@@ -1,9 +1,24 @@
+const {
+    getAllCustomers,
+    getCustomerById,
+    addNewCustomer,
+    updateCustomer,
+} = require('../../controllers/categories')
+
 
 module.exports = (router) => {
     // GET: list of all the customers
-    router.get('/customers', (req, res) => {
-        return res.json({ message: 'GET: list of all the customers' })
-    })
+    router.get('/customers', getAllCustomers)
+
+    // GET: a customer by id
+    router.get('/customers/:id', getCustomerById)
+
+    // POST: a new customer
+    router.post('/customers/new', addNewCustomer)
+
+    // PUT: update a customer by id
+    router.put('/customers/:id', updateCustomer)
 }
+
 
     
--- server/api/routes/products.js
+++ server/api/routes/products.js
@@ -1,7 +1,58 @@
+const {
+    getAllProducts,
+    addNewProduct,
+    updateProduct,
+    deleteProduct
+} = require('../../controllers/products');
+
+const {
+    getAllCategories,
+    addNewCategory,
+    updateCategory,
+    deleteCategory
+} = require('../../controllers/categories');
+
+const {
+    getAllRatingsAndReviews,
+    addNewRatingAndReview,
+    getRatingAndReviewById,
+    getRatingAndReviewByProductId
+} = require('../../controllers/ratingsAndReviews');
 
 module.exports = (router) => {
     // GET: list of all the products
-    router.get('/products', (req, res) => {
-        return res.json({ message: 'GET: list of all the products' })
-    })
+    router.get('/products', getAllProducts)
+
+    // POST: a new product
+    router.post('/products/new', addNewProduct)
+
+    // PUT: update a product by id
+    router.put('/products/:id', updateProduct)
+
+    // DELETE: delete a product by id
+    router.delete('/products/:id', deleteProduct)
+
+    // GET: list of all the categories
+    router.get('/products/categories', getAllCategories)
+
+    // POST: a new category
+    router.post('/products/categories/new', addNewCategory)
+
+    // PUT: update a category by id
+    router.put('/products/categories/:id', updateCategory)
+
+    // DELETE: delete a category by id
+    router.delete('/products/categories/:id', deleteCategory)
+
+    // GET: list of all the ratings and reviews
+    router.get('/products/ratingsAndReviews', getAllRatingsAndReviews)
+
+    // POST: a new rating and review
+    router.post('/products/ratingsAndReviews/new', addNewRatingAndReview)
+
+    // GET: a rating and review by id
+    router.get('/ratingsAndReviews/:id', getRatingAndReviewById)
+
+    // GET: a rating and review by product id
+    router.get('/ratingsAndReviews/product/:id', getRatingAndReviewByProductId)
 }
